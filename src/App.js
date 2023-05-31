37cbe508bf84f4e61fdbffeef607a374a0dcd8ef
Rajesh01-star <rajeshbehera489@gmail.com>
2023-05-31 19:12:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [uploadedImage, setUploadedImage] = useState(null);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const fileInput = document.getElementById('imageUpload');
+    const selectedFile = fileInput.files[0];
+
+    console.log('Selected file:', selectedFile);
+
+    setUploadedImage(URL.createObjectURL(selectedFile));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Upload Image Form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div className="form-group">
+          <label htmlFor="imageUpload">Select an image:</label>
+          <input type="file" id="imageUpload" accept="image/*" />
+        </div>
+        <button type="submit">Upload</button>
+      </form>
+      {uploadedImage && (
+        <div className="uploaded-image">
+          <h2>Uploaded Image:</h2>
+          <img src={uploadedImage} alt="Uploaded" />
+        </div>
+      )}
     </div>
   );
 }
